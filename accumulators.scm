82d330d2d1be8921d141fffa90e0b3de0fd035d4
Nicholas Gohman <nicholas.gohman@outlook.com>
2020-03-30 20:30:07 -0500

    Adds accumulators

---
+++ accumulators.scm
@@ -0,0 +1,74 @@
+;; ======================================================
+;;; Accumulation structures
+;; ======================================================
+
+(define (inc n) (+ n 1))
+(define (identity x) x)e
+(define (truth? x) true)
+
+;; Accumulation
+(define (filtered-accumulate predicate? combiner null-value term a next b)
+  (define (iter x result)
+    (cond ((> x b) result)
+	  ((predicate? x)
+	   (iter (next x) (combiner result (term x))))
+	  (else
+	   (iter (next x) result))))
+  (iter a null-value))
+
+(define (accumulate combiner null-value term a next b)
+  (filtered-accumulate truth? combiner null-value term a next b))
+
+(define (sum term a next b)
+  (accumulate + 0.0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1.0 term a next b))
+
+(define (factorial n)
+  (if (or (= n 1) (= n 0))
+      1
+      (product identity 1 inc n)))
+
+;; Sum of cubes
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+;; Sum of integers
+(define (sum-ints a b)
+  (sum identity a inc b))
+
+;; ======================================================
+;;; Integrations
+;; ======================================================
+
+;; Using the definition of the integral
+(define (definite-integral f a b dx)
+  (define (add-dx a)
+    (+ a dx))
+  (* (sum f (+ a (/ dx 2)) add-dx b)
+     dx))
+
+;; Using Simpson's Rule
+(define (simpson-integral f a b n)
+  (define (simpson-next x)
+    (+ x (/ (- b a) n)))
+  (define (simpson-term x)
+    (define (k)
+      (/ (* n (- x a)) (- b a)))
+    (define (simpson-const k)
+      (cond ((or (= k 0) (= (- n k) 0)) 1)
+	    ((and (> (- n k) 2) (even? k))
+	     (+ (remainder k 2) 2))
+	    ((> (- n k) 2)
+	     (+ (remainder k 2) 3))
+	    ((= (- n k) 2) 2)
+	    (else 4)))
+    (* (simpson-const (k)) (f x)))
+  (* (/ (- b a) (* 3.0 n))
+     (sum simpson-term a simpson-next b)))
+
+;; Integral alias
+(define (integral f a b n) (simpson-integral f a b n))
+
+
