c7a7d7d760a48173a76d71a85077aae51d3d122d
Nicholas Gohman <nicholas.gohman@outlook.com>
2020-04-01 07:19:19 -0500

    Adds estimations of important constants

--- highmath.scm
+++ highmath.scm
@@ -10,13 +10,8 @@
 (load "general_methods.scm")  ; Resolve dependencies
 
 ;; ======================================================
-; Higher level mathematics procedures
+; Additional structures, general methods
 ;; ======================================================
-
-;; Square root
-(define (sqrt x)
-  (fixed-point (lambda (y) (average y (/ x y)))
-	       1.0))
 
 ;; Series sum
 (define (sum term a next b)
@@ -26,10 +21,69 @@
 (define (product term a next b)
   (accumulate * 1.0 term a next b))
 
+;; Iterative
+(define (cont-frac n d k)
+  (define (iter i val)
+    (if (= i 0)
+	val
+	(iter (- i 1)
+	      (/ (n i) (+ (d i) val)))))
+  (iter (- k 1)
+	(/ (n k) (d k))))
+
+;; ======================================================
+; Higher level mathematics procedures
+;; ======================================================
+
+;; Square root
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+	       1.0))
+
+;; Cube root
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+	       1.0))
+
 (define (factorial n)
   (if (or (= n 1) (= n 0))
       1
       (product identity 1 inc n)))
+
+;; ======================================================
+; Estimations of some useful constants
+;; ======================================================
+
+(define (pi) (half-interval-method sin 3.1 3.2))
+
+;; Golden ratio
+(define (phi)
+  (fixed-point
+   (average-damp (lambda (x) (+ 1 (/ 1 x))))
+   1.0))
+		   
+;; Base of the natural logarithm
+(define (e)
+  (+ 2
+     (cont-frac
+      (lambda (i) 1.0)
+      (lambda (i)
+	(cond ((= i 1) 1)
+	      ((= (remainder (+ i 1) 3) 0)
+	       (* (/ (+ i 1) 3) 2))
+	      (else 1)))
+      10)))
+
+;; Estimated tangent function
+(define (tan-cf x k)
+  (cont-frac
+   (lambda (i)
+     (if (= i 1) x
+	 (* -1 (square x))))
+   (lambda (i)
+     (if (= i 1) 1
+	 (- (* i 2) 1)))
+   k))
 
 ;; ======================================================
 ; Integrations
