c0d410b54fd824a170eb7415ee48d18b5de438b0
Nicholas Gohman <nicholas.gohman@outlook.com>
2020-04-06 07:28:41 -0500

    Fixes and improvements to the base code

--- assignments/twenty-one/base.scm
+++ assignments/twenty-one/base.scm
@@ -7,17 +7,23 @@
                       (make-new-hand (deal))
                       (hand-up-card house-initial-hand))))
       (if (> (hand-total player-hand) 21)
-          0                                ; ``bust'': player loses
+          (bust (hand-total player-hand) 0)                ; ``bust'': player loses
           (let ((house-hand 
                  (play-hand house-strategy
                             house-initial-hand
                             (hand-up-card player-hand))))
             (cond ((> (hand-total house-hand) 21)
-                   1)                      ; ``bust'': house loses
+                   (bust (hand-total house-hand) 1))       ; ``bust'': house loses
                   ((> (hand-total player-hand)
                       (hand-total house-hand))
                    1)                      ; house loses
-                  (else 0)))))))           ; player loses
+                  (else 0)))))))        ; player loses
+
+(define (bust total condition)
+  (display "Bust at: ")
+  (write total)
+  (newline)
+  condition)
 
 (define (play-hand strategy my-hand opponent-up-card)
   (cond ((> (hand-total my-hand) 21) my-hand) ; I lose... give up
@@ -48,16 +54,20 @@
 
 (define (hit? your-hand opponent-up-card)
   (newline)
-  (princ "Opponent up card ")
-  (princ opponent-up-card)
+  (display "Opponent up card ")
+  (write opponent-up-card)
   (newline)
-  (princ "Your Total: ")
-  (princ (hand-total your-hand))
+  (display "Your Total: ")
+  (write (hand-total your-hand))
   (newline)
-  (princ "Hit? ")
+  (display "Hit? ")
   (user-says-y?))
 
 
-(define (user-says-y?) (eq? (read-from-keyboard) 'y))
+(define (user-says-y?)
+  (newline)
+  (let ((user-input (read-line (current-input-port))))
+    (string=? user-input "y")))
+
 
 
