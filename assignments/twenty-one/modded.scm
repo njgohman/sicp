8cb7da65d5f1a43e2ef4b3a43f3ccfb59216140b
Nicholas Gohman <nicholas.gohman@outlook.com>
2020-04-06 07:53:20 -0500

    Completes problem 3

--- assignments/twenty-one/modded.scm
+++ assignments/twenty-one/modded.scm
@@ -2,9 +2,17 @@
 
 (load "base.scm")
 
-;; The simple stop strategy
+;; The simple stop strategy, problem 2
 (define (stop-at n)
   (lambda (hand up-card)
     (if (< (hand-total hand) n)
 	true
 	false)))
+
+;; Testing multiple strategies, problem 3
+(define (test-strategy player-strategy house-strategy n)
+  (define (iter wins i)
+    (if (= i 0)
+	wins
+	(iter (+ wins (twenty-one player-strategy house-strategy)) (- i 1))))
+  (iter 0 n))
