a53d178fa9c37c5e90b39fd3f2e783bc1fa83484
Nicholas Gohman <nicholas.gohman@outlook.com>
2020-04-08 07:43:45 -0500

    Adds sicp graphing assignment

---
+++ assignments/graphing/base/scratch.scm
@@ -0,0 +1,15 @@
+
+;; --------------------------------------------------------------------------------
+;; Scratchpad for graphing assignment
+
+(define (make-point x y)
+  (lambda (bit)
+    (if (zero? bit) x y)))
+
+(define (x-of point)
+  (point 0))
+
+(define (y-of point)
+  (point 1))
+
+
